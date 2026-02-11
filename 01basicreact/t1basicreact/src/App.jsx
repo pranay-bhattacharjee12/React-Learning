fdd0c9f9d9292656fea67362cf6658aceff51844
pranay-bhattacharjee12 <pranaybhattacharjee66@gmail.com>
2026-02-12 01:55:02 +0530

    finalcommit

--- 01basicreact/t1basicreact/src/App.jsx
+++ 01basicreact/t1basicreact/src/App.jsx
@@ -2,15 +2,17 @@ import Test from "./Test"   // Test -> component name, it should start with capi
 
 function App() {
 
+  const name = "shan"   //{var} -> evaluated expression omly witten in return statement, 
+  // it is used to display dynamic data in the UI, 
+  // it is also used to pass data to child components as props
+
   return (
 
     //that is called fragment in react, it is used to wrap multiple elements 
     //without adding extra nodes to the DOM
     <>
-
     <Test/>
-
-
+    <h1>Hello world, Welcome {name}!</h1>   
     </>
 
   )
